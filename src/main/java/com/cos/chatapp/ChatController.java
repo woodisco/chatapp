3c80b8fcb3e514949627f0b3a015bf300f4fbcae
woodisco <woodisco.official@gmail.com>
2023-04-27 16:53:46 +0900

    마지막 강의2(Whitelabel Error 포함)

--- src/main/java/com/cos/chatapp/ChatController.java
+++ src/main/java/com/cos/chatapp/ChatController.java
@@ -13,9 +13,10 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 @RestController // 데이터 리턴 서버
 public class ChatController {
+
     private final ChatRepository chatRepository;
 
-    // 귓속말
+    // 귓속말 할때 사용하면 되요!!
     @CrossOrigin
     @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
@@ -24,7 +25,7 @@ public class ChatController {
     }
 
     @CrossOrigin
-    @GetMapping(value = "chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
         return chatRepository.mFindByRoomNum(roomNum)
                 .subscribeOn(Schedulers.boundedElastic());
@@ -32,8 +33,8 @@ public class ChatController {
 
     @CrossOrigin
     @PostMapping("/chat")
-    public Mono<Chat> setMsg(@RequestBody Chat chat) {
+    public Mono<Chat> setMsg(@RequestBody Chat chat){
         chat.setCreatedAt(LocalDateTime.now());
-        return chatRepository.save(chat);
+        return chatRepository.save(chat); // Object를 리턴하면 자동으로 JSON 변환 (MessageConverter)
     }
 }
